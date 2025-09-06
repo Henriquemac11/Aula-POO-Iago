a19da60b32bef98d238f56b99d544dfdb166dc70
Henrique Maciel <164898450+Henriquemac11@users.noreply.github.com>
2025-09-06 10:46:49 -0300

    Create Viagem.java

---
+++ Viagem.java
@@ -0,0 +1,13 @@
+
+public class Viagem {
+	private String destino;
+	private String dataDeIdaEVolta;
+	private String meioDeTransporte;
+	private String acomodacao;
+	private String roteiro;
+	private String atracao;
+	private String clima;
+	private String moedaLocal;
+	private String documentacao;
+	private boolean seguroViagem;
+}
