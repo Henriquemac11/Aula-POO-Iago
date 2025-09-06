fedcd13b23e02116eaa1ba5284e956d8f31f172a
Henrique Maciel <164898450+Henriquemac11@users.noreply.github.com>
2025-09-06 10:46:04 -0300

    Create Restaurante.java

---
+++ Restaurante.java
@@ -0,0 +1,13 @@
+
+public class Restaurante {
+	private String nome;
+	private String endereco;
+	private int telefone;
+	private String tipoDeCulinaria;
+	private float faixaDePreco;
+	private String horaFuncionamento;
+	private String ambiente;
+	private boolean aceitaCredito;
+	private boolean possuiVegano;
+	private boolean reservas;
+}
