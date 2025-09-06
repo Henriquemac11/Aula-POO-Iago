a01cfd4906f21df33673e0bb4b6a0787c843e494
Henrique Maciel <164898450+Henriquemac11@users.noreply.github.com>
2025-09-06 10:45:37 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,13 @@
+
+	public class Pessoa {
+		private String nome;
+		private String data;
+		private String endereco;
+		private int telefone;
+		private String email;
+		private int cpf;
+		private int rg;
+		private String estadocivil;
+		private String profissao;
+		private String hobbies;
+}
