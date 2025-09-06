5d0e7bf9519302f0feb99585fa3cdaac606110eb
Henrique Maciel <164898450+Henriquemac11@users.noreply.github.com>
2025-09-06 10:45:14 -0300

    Create Carro.java

---
+++ Carro.java
@@ -0,0 +1,14 @@
+
+public class AnimalDeEstimacao {
+	private String especie;
+	private String raca;
+	private String nome;
+	private int idade;
+	private String sexo;
+	private String porte;
+	private String castrado;
+	private boolean vacinaEmDia;
+	private boolean nessecidadeEspecial;
+	private String personalidade;
+	
+}
